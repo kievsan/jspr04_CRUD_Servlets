fa6463de6ee729fafb064e2b3c2ac2a26bc37e88
kievskiy <knhel7@mail.ru>
2023-12-26 21:24:48 +0300

    Init constants

---
+++ src/main/java/ru/mail/knhel7/Const.java
@@ -0,0 +1,12 @@
+package ru.mail.knhel7;
+
+public abstract class Const {
+    public static final String DELETE = "DELETE";
+    public static final String GET = "GET";
+    public static final String POST = "POST";
+
+    public static final String PATH = "/api/posts";
+    public static final String PATH_WITH_ID = PATH + "/\\d+";
+
+    public static final String APP_JSON = "application/json";
+}
