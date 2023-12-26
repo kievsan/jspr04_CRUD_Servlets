7b797d5f640c144acd326f66c6e145ff8b1d333d
kievskiy <knhel7@mail.ru>
2023-12-26 20:23:47 +0300

    Initial

---
+++ src/main/java/ru/mail/knhel7/service/PostService.java
@@ -0,0 +1,32 @@
+package ru.mail.knhel7.service;
+
+import ru.mail.knhel7.exception.NotFoundException;
+import ru.mail.knhel7.model.Post;
+import ru.mail.knhel7.repository.PostRepository;
+
+import java.util.List;
+
+public class PostService {
+  private final PostRepository repository;
+
+  public PostService(PostRepository repository) {
+    this.repository = repository;
+  }
+
+  public List<Post> all() {
+    return repository.all();
+  }
+
+  public Post getById(long id) {
+    return repository.getById(id).orElseThrow(NotFoundException::new);
+  }
+
+  public Post save(Post post) {
+    return repository.save(post);
+  }
+
+  public void removeById(long id) {
+    repository.removeById(id);
+  }
+}
+
